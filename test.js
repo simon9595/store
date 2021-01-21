b9fc7adeb7a29a95da1832aa23748171d29e64fd
simon9595 <szymon1995@gmx.com>
2021-01-21 11:34:47 +0100

    Initial commit

--- test.js
+++ test.js
@@ -7,26 +7,23 @@
     item page, maybe both?
 }*/
 
-const productImage = document.getElementsByClassName('product-image')
-const ProductName = document.getElementsByClassName('product-name')
-const addToCart = document.getElementsByClassName('add-to-cart')
-const price = document.getElementsByClassName('price')
-const itemDescription = document.getElementsByClassName('item-description')
+const products = document.getElementsByClassName('products');
+const productLink = document.getElementById('product-link')
+const product = document.getElementById('item');
+const productImage = document.getElementsByClassName('product-image');
+const ProductName = document.getElementsByClassName('product-name');
+const addToCart = document.getElementsByClassName('add-to-cart');
+const price = document.getElementsByClassName('price');
+const itemDescription = document.getElementsByClassName('item-description');
 
-let newContainer = document.createElement('div');
+console.log(typeof products);
+
+let newItem = document.createElement('article');
 let newImage = document.createElement('img');
-let newTitle = document.createElement('h2');
+let newName = document.createElement('h2');
 let newText = document.createElement('p');
-
-// class item {
-//     constructor(imageUrl, name, price, description, colors) {
-//         this.imageUrl = imageUrl;
-//         this.name = name;
-//         this.price = price;
-//         this.description = description;
-//         this.colors = colors;
-//     }
-// }
+let newPrice = document.createElement('p');
+let newDescription = document.createElement('p');
 
 let apiRequest = new XMLHttpRequest();
 apiRequest.open('GET', 'http://localhost:3000/api/teddies'); 
@@ -42,8 +39,59 @@ apiRequest.onreadystatechange = () => {
         // console.log(response)
 
         for(let i = 0; i < response.length; i++) {
-            console.log(response[i])
-            // newTitle = response[i];
+            let colorArray = response[i];
+            for(let j = 0; j < response[i].colors.length; j++) {
+                console.log(colorArray[j]);
+            } //??
+            // console.log(response[i]);
+            // console.log(response[i].imageUrl)
+            // console.log(response[i].name);
+            // console.log(response[i].description)
+            // console.log(response[i]._id)
+            // console.log(response[i].price)
+            console.log(response[i].colors.length)
+
+            newImage = response[i].imageUrl;
+            console.log(newImage);
+            
+            newName = response[i].name;
+            console.log(newName); //works
+
+            newPrice = response[i].price;
+            console.log(newPrice)
+
+            newDescription = response[i].description;
+            console.log(newDescription);
+
+            // broken
+            newName.textContent = response[i].name;
+            console.log(newName);
+
+            newDescription.textContent = response[i].description;
+            console.log(newDescription);
+
+            newItem.append(newName);
+            newItem.append(newDescription);
+
+            console.log(newItem);
+
+            newItem.classList.add('card-body');
+
+            products.appendChild(newItem);
+
+
+            // console.log(newName);
+            // newItem.appendChild(newImage);
+            // newItem.appendChild(newName);
+            // newItem.appendChild(newPrice);
+            // newItem.appendChild(newDescription);
+
+            // newItem.classList.add("card")
+            
+            // for(let j = 0; j < response[i].colors.length; j++) {
+            //     console.log(response[j]);
+            // }
+
             // for loop for colors as well
             // has to be executed after server response
         // have a loop for populating the list
